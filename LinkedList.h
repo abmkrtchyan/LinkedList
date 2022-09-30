cc8e68fcbf5fb10a1f6304b61049e2828cc2c583
Albert Mkrtchyan <albert.mkrtchyan.98@mail.ru>
2022-09-30 12:28:16 -0700

    add merge lists

--- LinkedList.h
+++ LinkedList.h
@@ -5,15 +5,17 @@
 
 template<class T>
 class LinkedList {
-private:
+public:
     LinkedListNode<int> *head;
     LinkedListNode<int> *tail;
-public:
+
     LinkedList() : head(nullptr), tail(nullptr) {}
 
     void insert(LinkedListNode <T> *node);
 
     void insert(const T &data);
+
+    static void merge(LinkedList<int>,LinkedList<int>);
 
     void print() {
         LinkedListNode <T> *node = this->head;
@@ -41,4 +43,31 @@ template<class T>
 void LinkedList<T>::insert(const T &data) {
     auto *node = new LinkedListNode<T>(data);
     insert(node);
+}
+
+static void merge(LinkedList<int> &list1,LinkedList<int> &list2){
+    LinkedListNode <int>* node1 = list1.head;
+    LinkedListNode <int>* node2 = list2.head;
+    LinkedList <int> mergedList;
+    while(node1 && node2){
+        if(node1->data<=node2->data){
+            mergedList.insert(node1->data);
+            node1 = node1->next;
+        }
+        else {
+            mergedList.insert(node2->data);
+            node2 = node2->next;
+        }
+    }
+    while(node1){
+            mergedList.insert(node1->data);
+            node1 = node1->next;
+        }
+    while(node2){
+        mergedList.insert(node2->data);
+        node2 = node2->next;
+    }
+
+
+
 }

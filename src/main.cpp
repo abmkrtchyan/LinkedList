c1ea14140ad33c033981e7033ac2faaf9719fcec
abmkrtchyan <abmkrtchyan@ispras.ru>
2023-09-29 16:21:32 +0400

    add tests

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,18 @@
+#include <vector>
 #include "algorithm"
 #include "LinkedList.h"
 
+void test_iterator();
+
+void test_merge();
+
 int main() {
+    test_merge();
+    test_iterator();
+    return 0;
+}
+
+void test_merge() {
     LinkedList<int> list1;
     list1.push_back(1);
     list1.push_back(3);
@@ -31,9 +42,38 @@ int main() {
         std::cout << it << " -> ";
     }
     std::cout << "/\n";
-    auto xx = find(list1.begin(), list1.end(), 3);
-    if (xx != list1.end()) {
-        std::cout << *xx << std::endl;
+}
+
+void test_iterator() {
+    LinkedList<int> list1;
+    list1.push_back(1);
+    list1.push_back(3);
+    list1.push_back(5);
+    list1.push_back(7);
+    list1.push_back(9);
+    for (auto it: list1) {
+        std::cout << it << " -> ";
     }
-    return 0;
+    std::cout << "/\n";
+
+    std::vector<int> arr;
+    arr.push_back(2);
+    arr.push_back(4);
+    arr.push_back(6);
+    arr.push_back(8);
+    arr.push_back(10);
+    for (auto it: arr) {
+        std::cout << it << " -> ";
+    }
+    std::cout << "/\n";
+
+    auto list_iter = find(list1.begin(), list1.end(), 3);
+    if (list_iter != list1.end()) {
+        std::cout << *list_iter << std::endl;
+    }
+
+    auto vector_iter = find(arr.begin(), arr.end(), 10);
+    if (vector_iter != arr.end()) {
+        std::cout << *vector_iter << std::endl;
+    }
 }
